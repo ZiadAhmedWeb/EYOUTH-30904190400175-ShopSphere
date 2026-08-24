dd54ca0ba2e93d6bb6d95570e3bbf36f86534088
Ziad Ahmed <ziadahmedweb@gmail.com>
2026-08-24 21:42:59 +0300

    Ship uploads folder into serverless bundle via includeFiles

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,13 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-const uploadsDir = path.join(__dirname, '..', 'uploads');
-try {
-  fs.readdirSync(uploadsDir);
-} catch (err) {
-  console.error('uploads dir unavailable:', err.message);
-}
-
 const app = require('../index');
 
 module.exports = app;
